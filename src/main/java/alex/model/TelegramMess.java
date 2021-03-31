8e4af79f0e0613bf42852b16bac3db0b76dd49c6
Vladislav Vekshin <vladik201518@gmail.com>
2021-03-31 20:52:22 +0300

    change var names

--- src/main/java/alex/model/TelegramMess.java
+++ src/main/java/alex/model/TelegramMess.java
@@ -4,14 +4,14 @@ import java.util.Date;
 
 public class TelegramMess {
     private long mess_id;
-    private String mess_content;
-    private int mess_date;
+    private String mess_text;
+    private int mess_time;
     private String mess_type;
 
-    public TelegramMess(long mess_id, String mess_content, int mess_date, String mess_type) {
+    public TelegramMess(long mess_id, String mess_text, int mess_time, String mess_type) {
         this.mess_id = mess_id;
-        this.mess_content = mess_content;
-        this.mess_date = mess_date;
+        this.mess_text = mess_text;
+        this.mess_time = mess_time;
         this.mess_type = mess_type;
     }
 
@@ -19,12 +19,12 @@ public class TelegramMess {
         return mess_id;
     }
 
-    public String getMess_content() {
-        return mess_content;
+    public String getMess_text() {
+        return mess_text;
     }
 
-    public int getMess_date() {
-        return mess_date;
+    public int getMess_time() {
+        return mess_time;
     }
 
     public String getMess_type() {
