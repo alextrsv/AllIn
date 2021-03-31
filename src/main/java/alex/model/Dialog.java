6d6a0a6a3adce0cf96d93aff9a44850fd505a5e9
Vladislav Vekshin <vladik201518@gmail.com>
2021-03-31 17:04:14 +0300

    adding telegramController, TelegramHandler, pom dependencies, pojo files

---
+++ src/main/java/alex/model/Dialog.java
@@ -0,0 +1,85 @@
+package alex.model;
+
+import java.util.Date;
+
+public class Dialog {
+    private Long id;
+    private String title;
+    private String icon;
+    private String note = "empty";
+    private String lastMsg_text = "";
+    private Date lastMsg_date;
+    private String lastMsg_type;
+    private boolean is_favorite = false;
+    private String type = "ordinary";
+    private int photo_id;
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getPhoto_id() {
+        return photo_id;
+    }
+
+    public void setPhoto_id(int photo_id) {
+        this.photo_id = photo_id;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public String getLastMsg_text() {
+        return lastMsg_text;
+    }
+
+    public Date getLastMsg_date() {
+        return lastMsg_date;
+    }
+
+    public boolean isIs_favorite() {
+        return is_favorite;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setLastMsg_text(String lastMsg_text) {
+        this.lastMsg_text = lastMsg_text;
+    }
+
+    public void setLastMsg_date(Date lastMsg_date) {
+        this.lastMsg_date = lastMsg_date;
+    }
+
+    public Dialog(Long id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public Dialog(Long id, String title, String icon, String lastMsg_text, Date lastMsg_date) {
+        this.id = id;
+        this.title = title;
+        this.icon = icon;
+        this.lastMsg_text = lastMsg_text;
+        this.lastMsg_date = lastMsg_date;
+    }
+}
